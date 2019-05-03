f3666b4003bc5b89342e9b43c0deea60a068dd50
Rob <betoarpi@gmail.com>
2019-05-02 22:18:34 -0500

    Se quitó del .gitignore la carpeta build para poder crear una Github page del proyecto.

---
+++ build/precache-manifest.4764135f7983929237d70320e9bfaf4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364e6448df56c48a5ba6ac1d94b3f4fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "771d38dad7b733578de7",
+    "url": "/static/css/main.9f753317.chunk.css"
+  },
+  {
+    "revision": "c2bef4fd89e10852a458",
+    "url": "/static/js/2.5d86a196.chunk.js"
+  },
+  {
+    "revision": "771d38dad7b733578de7",
+    "url": "/static/js/main.97facff5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
